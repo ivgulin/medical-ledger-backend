8b3c711e34aeef1d6fa909fac73a5795e12a0313
Ivan Gulin <boringnameishere@gmail.com>
2019-10-17 22:30:02 +0300

    provided CD/CI

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,93 @@
+def eurekaServer = new Structure(
+        "eureka-server",
+        "structural/eureka-server/build/Dockerfile")
+def authService = new Structure(
+        "auth-service",
+        "structural/auth-service/build/Dockerfile")
+def zuulGateway = new Structure(
+        "zuul-gateway",
+        "structural/zuul-gateway/build/Dockerfile")
+def documentationService = new Structure(
+        "documentation-service",
+        "structural/documentation-service/build/Dockerfile")
+def userService = new Structure(
+        "user-service",
+        "business/user-service/build/Dockerfile")
+def testService = new Structure(
+        "test-service",
+        "business/test-service/build/Dockerfile")
+
+def modules = [
+        eurekaServer,
+        zuulGateway,
+        authService,
+        documentationService,
+        userService,
+        testService
+]
+
+pipeline {
+    agent any
+
+    stages {
+        stage("maven build") {
+            steps {
+                println 'stage: maven build'
+                withMaven(maven: 'maven3') {
+                    sh 'mvn -B -Dmaven.test.skip=true clean package'
+                }
+            }
+        }
+
+        stage('deploy') {
+            steps {
+                println 'stage: deploy'
+                script {
+                    def serviceName
+
+                    def removeContainerCommand
+                    def removeImageCommand
+                    def buildCommand
+                    def runCommand
+
+                    String gitChanges = sh (
+                            script: 'git diff --name-only HEAD^ HEAD',
+                            returnStdout: true
+                    ).trim()
+                    println gitChanges
+
+                    for (int i = 0; i < modules.size(); i++) {
+                        serviceName = modules[i].serviceName
+                        if (gitChanges.contains(serviceName)) {
+                            removeContainerCommand = 'sudo docker rm \$(sudo docker stop ' + serviceName + ')'
+                            println removeContainerCommand
+                            sh removeContainerCommand
+
+                            removeImageCommand = 'sudo docker rmi medical-ledger/' + serviceName
+                            println removeImageCommand
+                            sh removeImageCommand
+
+                            buildCommand = 'sudo docker build -t medical-ledger/' + serviceName + ' -f ' + modules[i].dockerPath + ' .'
+                            println buildCommand
+                            sh buildCommand
+
+                            runCommand = 'sudo docker run --name ' + serviceName + ' --net=host medical-ledger/' + serviceName + ' &'
+                            println runCommand
+                            sh runCommand
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Structure {
+    String serviceName
+    String dockerPath
+
+    Structure(String serviceName, String dockerPath) {
+        this.dockerPath = dockerPath
+        this.serviceName = serviceName
+    }
+}
