18156e41ded348194374eb3d0110026d53df1ca7
Ivan Gulin <boringnameishere@gmail.com>
2019-11-19 15:15:25 +0200

    added basic auth config

---
+++ structural/zuul-gateway/src/main/java/com/mokujin/zuul/config/BasicAuthorizationSecurityConfig.java
@@ -0,0 +1,65 @@
+package com.mokujin.zuul.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+@EnableWebSecurity
+@Order(2)
+public class BasicAuthorizationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${auth.server.clientId}")
+    private String clientId;
+
+    @Value("${auth.server.clientSecret}")
+    private String clientSecret;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .requestMatcher(new BasicRequestMatcher())
+                .authorizeRequests()
+                .antMatchers("/auth/**", "/gov/**").authenticated()
+                .antMatchers(POST,"/user/registration/create-wallet").authenticated()
+                .anyRequest().denyAll()
+                .and().httpBasic()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authentication) throws Exception {
+        authentication.inMemoryAuthentication()
+                .withUser(clientId)
+                .password(passwordEncoder().encode(clientSecret))
+                .authorities("web-client");
+    }
+
+    private static class BasicRequestMatcher implements RequestMatcher {
+        @Override
+        public boolean matches(HttpServletRequest request) {
+            String auth = request.getHeader("Authorization");
+            return (auth != null && auth.startsWith("Basic"));
+        }
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- structural/zuul-gateway/src/main/java/com/mokujin/zuul/config/ResourceServerConfig.java
+++ structural/zuul-gateway/src/main/java/com/mokujin/zuul/config/ResourceServerConfig.java
@@ -3,12 +3,13 @@ package com.mokujin.zuul.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
-import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableResourceServer
@@ -25,10 +26,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/docs/**").permitAll()
-                .antMatchers(POST, "/auth/**", "/user/registration/create-wallet").anonymous()
+                .antMatchers("/auth/**", "/gov/**").denyAll()
+                .antMatchers(POST,"/user/registration/create-wallet").denyAll()
                 .anyRequest().authenticated();
     }
 
